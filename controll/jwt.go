2d5334173cddb42a52160615bdc49d1585a9773f
subkhiyoga <subkhiyoga28@gmail.com>
2023-04-15 05:17:45 +0700

    auth-jwt

--- controll/jwt.go
+++ controll/jwt.go
@@ -1,4 +1,4 @@
-package controller
+package controll
 
 import (
 	"net/http"
--- router/router.go
+++ router/router.go
@@ -4,7 +4,7 @@ import (
 	"log"
 
 	"github.com/gin-gonic/gin"
-	"github.com/subkhiyoga/auth-jwt/controller"
+	"github.com/subkhiyoga/auth-jwt/controll"
 	"github.com/subkhiyoga/auth-jwt/database"
 	"github.com/subkhiyoga/auth-jwt/repository"
 	"github.com/subkhiyoga/auth-jwt/usecase"
@@ -13,14 +13,14 @@ import (
 func Run() {
 	db := repository.ConnectDB()
 	secret_key := []byte(database.DotEnv("SECRET_KEY"))
-	authMiddleware := controller.AuthMiddleware(secret_key)
+	authMiddleware := controll.AuthMiddleware(secret_key)
 
 	// gin router
 	router := gin.Default()
 
 	mahasiwaRepo := repository.NewMahasiswaRepo(db)
 	loginUsecase := usecase.NewLoginUsecase(mahasiwaRepo)
-	loginJwt := controller.NewCredentialsJwt(loginUsecase)
+	loginJwt := controll.NewCredentialsJwt(loginUsecase)
 
 	// set up routes for login
 	router.POST("/login", authMiddleware, loginJwt.Login)
